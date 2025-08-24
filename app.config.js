aae64a97ac0a5e7a3de5310528b82baf1f702313
steviegsb-ctrl <stevieg.sb@googlemail.com>
2025-08-24 13:31:00 +0100

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,39 +1,38 @@
-/** Expo app config for Chore Beasts */
-module.exports = {
+export default {
   expo: {
     name: "Chore Beasts",
     slug: "chore-beasts",
     version: "1.0.0",
     orientation: "portrait",
-
     icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
     splash: {
       image: "./assets/splash.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff"
     },
-
-    assetBundlePatterns: ["**/*"],
-
+    assetBundlePatterns: [
+      "**/*"
+    ],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "com.stevie.chorebeasts"
+      bundleIdentifier: "com.steviegsb.chorebeasts"
     },
-
     android: {
-      package: "com.stevie.chorebeasts",
+      package: "com.steviegsb.chorebeasts",
+      versionCode: 1,
       adaptiveIcon: {
         foregroundImage: "./assets/icon.png",
         backgroundColor: "#ffffff"
       }
     },
-
     web: {
       favicon: "./assets/icon.png"
     },
-
-    experiments: {
-      tsconfigPaths: false
+    extra: {
+      eas: {
+        projectId: "your-expo-project-id"
+      }
     }
   }
 };
