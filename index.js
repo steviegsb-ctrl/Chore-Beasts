9b543f9a37e4d8d9637cf75c23cd922acc21a99f
steviegsb-ctrl <stevieg.sb@googlemail.com>
2025-08-31 19:07:44 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import { registerRootComponent } from "expo";
+import App from "./App";
+
+// registerRootComponent ensures the environment is set up
+// correctly whether running in Expo Go or a native build.
+registerRootComponent(App);
