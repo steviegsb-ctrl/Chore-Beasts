762a65685cdf95f1d143a09f531daf3806154ff6
steviegsb-ctrl <stevieg.sb@googlemail.com>
2025-08-31 12:45:49 +0100

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function HomeScreen({ xp }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 28, fontWeight: "800" }}>Chore Beasts 🐉</Text>
+      <Text style={{ fontSize: 28, fontWeight: "bold" }}>Chore Beasts 🧹</Text>
       <Text style={{ marginTop: 8, fontSize: 18 }}>XP: {xp}</Text>
     </View>
   );
