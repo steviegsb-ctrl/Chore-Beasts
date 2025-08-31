2946deb743c6a2ba0b006b5f3a062fe0006a28ec
steviegsb-ctrl <stevieg.sb@googlemail.com>
2025-08-31 11:14:47 +0100

    Update ChoresScreen.js

--- src/screens/ChoresScreen.js
+++ src/screens/ChoresScreen.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
 
-export default function ChoresScreen() {
+export default function HomeScreen({ xp }) {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24 }}>Chores List 🧹</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 28, fontWeight: "800" }}>Chore Beasts 🐉</Text>
+      <Text style={{ marginTop: 8, fontSize: 18 }}>XP: {xp}</Text>
     </View>
   );
 }
